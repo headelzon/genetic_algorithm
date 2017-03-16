10ee74b7d09720207bf6b021929f032688673159
headelzon <w.koziak@aol.com>
2017-03-16 17:27:58 +0100

    Mating in loop until all elements the same

--- Genetic_algorithm.m
+++ Genetic_algorithm.m
@@ -14,27 +14,36 @@ average_ff_init = mean(ff_init)    % average fitness
 ff = ff_init;
 ff_total = ff_total_init;
 
-prob = ff/ff_total;         % probability of selection
+iterations = 0;
 
-[prob, A] = sort(prob);
-probabilities = [prob, A]; %probabilities(1,2) - 1 row, 2 col
+while all(ff == ff(1)) == 0
 
-C = [];
-min = 0;
+    prob = ff/ff_total;         % probability of selection
 
-for n=1:6
-    C = [C; min, min+prob(n)];
-    min = min + prob(n);
+    [prob, A] = sort(prob);
+    probabilities = [prob, A]; %probabilities(1,2) - 1 row, 2 col
+
+    C = [];
+    min = 0;
+
+    for n=1:6
+        C = [C; min, min+prob(n)];
+        min = min + prob(n);
+    end
+
+    C = [C, A]; 
+    % matrix C with selection intervals (columns 1 and 2 which take into consideration
+    % selection probability, and the 3rd column contains indices of corresponding individuals
+
+    [a, b] = select_individuals(C); % select individuals a and b for mating
+
+    x = mate(x, a, b);              % mate individuals a and b to form new population x from previous pop x
+
+    ff = sum(x, 2);
+    ff_total = sum(ff)
+    
+    iterations = iterations + 1;
 end
 
-C = [C, A]; 
-% matrix C with selection intervals (columns 1 and 2 which take into consideration
-% selection probability, and the 3rd column contains indices of corresponding individuals
-
-[a, b] = select_individuals(C); % select individuals a and b for mating
-
-x = mate(x, a, b);              % mate individuals a and b to form new population x from previous pop x
-
-ff = sum(x, 2)
-ff_total = sum(ff);
+ff
 average_ff = mean(ff)
